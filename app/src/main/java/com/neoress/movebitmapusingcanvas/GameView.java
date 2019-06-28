133c0c0e591b22eefd1d36c244ee2513b0d18741
Neoreul <neoreul.qn96@gmail.com>
2019-06-28 11:19:09 +0700

    Check collision wall

--- app/src/main/java/com/neoress/movebitmapusingcanvas/GameView.java
+++ app/src/main/java/com/neoress/movebitmapusingcanvas/GameView.java
@@ -4,33 +4,47 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Point;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
-public class TheChainView extends View {
+public class GameView extends View {
     Bitmap bitmap;
     float x = 200;
     float y = 200;
 
-    TheChainView(Context context, AttributeSet attrs) {
+    GameView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_angry);
+        bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_happy);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        float posX, posY;
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                x = event.getX();
-                y = event.getY();
+                posX = event.getX();
+                posY = event.getY();
+
+                if(posX >= 0 && posX < getWidth() - bitmap.getWidth() &&
+                    posY >= 0 && posY < getHeight() - bitmap.getHeight()) {
+                    x = posX;
+                    y = posY;
+                }
                 // redraw
                 invalidate();
                 break;
             case MotionEvent.ACTION_MOVE:
-                x = event.getX();
-                y = event.getY();
+                posX = event.getX();
+                posY = event.getY();
+
+                if(posX >= 0 && posX < getWidth() - bitmap.getWidth() &&
+                        posY >= 0 && posY < getHeight() - bitmap.getHeight()) {
+                    x = posX;
+                    y = posY;
+                }
                 // redraw
                 invalidate();
                 break;
